34171a24c8db3d9b83a0701d7d800677bd608dbd
UP240463 <up240463@alumnos.upa.edu.mx>
2025-02-20 12:15:03 -0600

    programas de python

---
+++ programa uno.py
@@ -0,0 +1,9 @@
+print ("hola mundo")
+"""
+hola, esto es un comentario mas paar hacer la programacion
+hdgufherg
+fklklgjjk
+jgrihigriughrvurrurihsioghposdfkhweioghsdiohweugwrhgiowrhgiwo
+jhgighithrgirhwitwethwetoiyeioghweioth4uuwtqntwtpeee
+tteiriyeriojoitjwoitrotiretyiohthrt
+"""
